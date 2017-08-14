b43bf2722495bdc4949deff560f11c2e4473e75c
unknown <j_cunanan05@yahoo.com>
2017-08-14 15:27:00 +0800

    Add sample test login successful route

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,9 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/login', function () {
+    return response()->json([
+        'success' => 'Login Successful!'
+    ]);
+});
