a7ab3c04c0ae81485b81edbd269d6fd911972967
Unknown <j_cunanan05@yahoo.com>
2017-09-13 22:39:15 +0800

    Add auth to UsersController
    Added authentication middleware to UsersController

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -16,7 +16,7 @@ class LoginController extends Controller
     public function __construct()
     {
         $this->middleware('guest')->except(['logout', 'authenticatedUser']);
-        $this->middleware('auth')->only('logout');
+        $this->middleware('auth.basic')->only('logout');
     }
 
     /** 
