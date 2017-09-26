4325f37f1b5ac5d1e6f9d1eb5ad9509dfbdb7950
unknown <j_cunanan05@yahoo.com>
2017-09-26 18:40:48 +0800

    Edit NavbarItem

---
+++ resources/assets/js/components/navbar/NavbarItem.vue
@@ -0,0 +1,15 @@
+<template>
+  <app-link class="navbar-item" 
+    @click="$emit('click')" >
+    <slot></slot>
+  </app-link>
+</template>
+
+<script>
+import '../link/Links.js';
+
+export default {
+  name: 'navbar-item'
+}
+</script>
+
